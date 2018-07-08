03d8ae67819690ad64c498c35529d2e9e8bbf557
Michael Dao <mike@turing.io>
2018-07-08 12:29:47 -0600

    initial working status with rails 5.2.0 and ruby 2.4.1

---
+++ config/routes.rb
@@ -0,0 +1,7 @@
+Rails.application.routes.draw do
+   root to: "welcome#index"
+   get '/login' => 'sessions#new'
+   get '/signup' => 'users#new'
+   post '/users' => 'users#create'
+   post '/login' => 'sessions#create'
+end
